05ed625b83efaa7471e719c33a6f1b933892394f
Konstantinos Charalampidis <konstantinos.charalampidis@bossinfo.ch>
2022-12-18 10:43:13 +0200

    working with supabase client on auth

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -3,39 +3,24 @@ import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
 
 const Login = () => {
-  const supabaseClient = useSupabaseClient();
+  const supabase = useSupabaseClient();
   const user = useUser();
   const [data, setData] = useState();
 
+  const signIn = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: "test.bosinfo.bs@gmail.com",
+      password: "Test123456",
+    });
+
+    console.log(error);
+  };
+
   useEffect(() => {
-    async function loadData() {
-      const { data } = await supabaseClient.from("test").select("*");
-      setData(data);
-    }
-    // Only run query once user is logged in.
-    if (user) loadData();
-  }, [user]);
+    signIn();
+  }, []);
 
-  if (!user)
-    return (
-      <Auth
-        redirectTo="http://localhost:3000/"
-        appearance={{ theme: ThemeSupa }}
-        supabaseClient={supabaseClient}
-        providers={["google", "github"]}
-        socialLayout="horizontal"
-      />
-    );
-
-  return (
-    <>
-      <button onClick={() => supabaseClient.auth.signOut()}>Sign out</button>
-      <p>user:</p>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <p>client-side data fetching with RLS</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
-  );
+  return <div>Login Page</div>;
 };
 
 export default Login;
